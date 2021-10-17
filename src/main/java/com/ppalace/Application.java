beae288a3b700ffafca77548c6aa6f177ea5b574
Pedro Palacios Rodriguez <ppalacios1>
2021-10-17 22:34:37 +0000

    Initial commit

---
+++ src/main/java/com/ppalace/Application.java
@@ -0,0 +1,10 @@
+package com.ppalace;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
